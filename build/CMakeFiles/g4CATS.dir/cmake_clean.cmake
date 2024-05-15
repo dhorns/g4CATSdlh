8b15451ced8afa503c6991bd778b92dc03fb3060
David Hornidge <dhornidge@mta.ca>
2024-05-15 12:18:38 -0300

    First commit

---
+++ build/CMakeFiles/g4CATS.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/g4CATS.dir/g4CATS.cc.o"
+  "CMakeFiles/g4CATS.dir/g4CATS.cc.o.d"
+  "CMakeFiles/g4CATS.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/g4CATS.dir/src/ActionInitialization.cc.o.d"
+  "CMakeFiles/g4CATS.dir/src/CalorHit.cc.o"
+  "CMakeFiles/g4CATS.dir/src/CalorHit.cc.o.d"
+  "CMakeFiles/g4CATS.dir/src/CalorimeterSD.cc.o"
+  "CMakeFiles/g4CATS.dir/src/CalorimeterSD.cc.o.d"
+  "CMakeFiles/g4CATS.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/g4CATS.dir/src/DetectorConstruction.cc.o.d"
+  "CMakeFiles/g4CATS.dir/src/EventAction.cc.o"
+  "CMakeFiles/g4CATS.dir/src/EventAction.cc.o.d"
+  "CMakeFiles/g4CATS.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/g4CATS.dir/src/PrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/g4CATS.dir/src/RunAction.cc.o"
+  "CMakeFiles/g4CATS.dir/src/RunAction.cc.o.d"
+  "g4CATS"
+  "g4CATS.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/g4CATS.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
